9d6f924dd9dd77222a070f543eac4334b1ea24d1
Andrey Negrash <andi.ne2002@gmail.com>
2022-08-22 16:36:35 +0300

    Initial commit

---
+++ src/src/methods/Secant.java
@@ -0,0 +1,17 @@
+package src.methods;
+
+import src.objects.AnswerX;
+import src.objects.Func;
+
+public class Secant {
+
+    public AnswerX calculate(Func function, double precision, double begin, double end, int iterationCount) {
+        double newBegin = begin - (function.calcFunc(begin)/(function.calcFunc(end) - function.calcFunc(begin)) *
+                (end - begin));
+        double diff = Math.abs(function.calcFunc(newBegin));
+
+        if ( diff < precision ||iterationCount > 50 ) return new AnswerX(newBegin, iterationCount);
+        return calculate(function, precision, newBegin, end, iterationCount + 1);
+    }
+}
+
---
+++ src/src/methods/Tangent.java
@@ -0,0 +1,16 @@
+package src.methods;
+
+import src.objects.AnswerX;
+import src.objects.Func;
+
+public class Tangent {
+
+    public AnswerX calculate(Func function, double precision, double approx, int iterationCount) {
+        double newApprox = approx - ( function.calcFunc(approx)/function.calcDer(approx) );
+        double diff = Math.abs(function.calcFunc(newApprox));
+
+        if ( diff < precision || iterationCount > 50 ) return new AnswerX(newApprox, iterationCount);
+        return calculate(function, precision, newApprox, iterationCount + 1);
+    }
+
+}
